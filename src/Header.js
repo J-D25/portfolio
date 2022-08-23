a94e9d9cf4f71b364be76c2dbe81cc5f966bb9b9
J-D25 <jd.auzanneau@icloud.com>
2022-08-23 19:02:55 +0200

    public/index.html, src/Header.js, src/index.css, src/index.js Add header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header(){
+const items = [{name:"Projets", link:"#"}, {name:"À propos", link:"#"}, {name:"Contact", link:"#"}];
+return (<>
+    <p className="Header-logo">Jessy-Daniel</p>
+    <ul>{
+        items.map((item, index) => <li key={index}><a href={item.link}>{item.name}</a></li>)
+    }</ul>
+</>);
+}
+
+export default Header;
