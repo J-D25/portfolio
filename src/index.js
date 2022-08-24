7e9ff0950472a513b6edc867be17c216ba05430c
J-D25 <jd.auzanneau@icloud.com>
2022-08-24 09:24:01 +0200

    src/index.js : Add Footer Component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Header';
 import App from './App';
+import Footer from './Footer';
 import reportWebVitals from './reportWebVitals';
 
 const header = ReactDOM.createRoot(document.getElementById('header'));
@@ -15,6 +16,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+const footer = ReactDOM.createRoot(document.getElementById('footer'));
+footer.render(
+  <React.StrictMode>
+    <Footer />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
