46a98ffbfd9239ae254eac79d39d3a7b9d9e6285
J-D25 <jd.auzanneau@icloud.com>
2022-08-25 13:52:36 +0200

    src/components/Project.js : Add Project page

---
+++ src/components/Project.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ProjectsCards from "./ProjectsCards";
+export default function Project() {
+  return (<>
+    <div className="App-content">
+      <h1>Mes projets</h1>
+      <ProjectsCards/>
+    </div>
+    </>);
+}
