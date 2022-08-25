dc0335b0e4736eca11bd44a9672509f7875fcb08
J-D25 <jd.auzanneau@icloud.com>
2022-08-25 13:35:36 +0200

    src/index.js : add routes

--- src/index.js
+++ src/index.js
@@ -3,21 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './components/Header';
 import App from './components/App';
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import About from "./components/About";
 import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const header = ReactDOM.createRoot(document.getElementById('header'));
 header.render(
   <React.StrictMode>
-    <Header />
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
+<React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 const footer = ReactDOM.createRoot(document.getElementById('footer'));
 footer.render(
   <React.StrictMode>
