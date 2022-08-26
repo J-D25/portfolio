d9ee14ee7091af94142981edb3b161ec5f1228cc
J-D25 <jd.auzanneau@icloud.com>
2022-08-26 13:43:37 +0200

    src/components/Footer.js : add <footer>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,12 @@ export default function Footer(){
 const items = [{name:"Mentions légales", link:"#"}, {name:"Plan du site", link:"#"}];
 
 return (<>
-<p className="Footer-copyright">2022, J-D25</p>
-<ul>{
+<footer id="footer">
+    <p className="Footer-copyright">2022, J-D25</p>
+    <ul>{
         items.map((item, index) => <li key={index}><a href={item.link}>{item.name}</a></li>)
     }</ul>
+</footer>
 </>
 )
 }
