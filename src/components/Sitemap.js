a7fa7a55d15dcda259243efa54a8dc97d4ead044
J-D25 <jd.auzanneau@icloud.com>
2022-08-29 11:22:33 +0200

    src/components/Sitemap.js : add Sitemap style

--- src/components/Sitemap.js
+++ src/components/Sitemap.js
@@ -1,11 +1,12 @@
 import './App.css';
+import './Sitemap.css';
 import {Link} from "react-router-dom";
 
 export default function Sitemap() {
   return (<>
     <div className="App-content">
       <h1>Plan du site</h1>
-      <ul>
+      <ul className="Sitemap-List">
         <li><Link to="/">Accueil</Link></li>
         <li><Link to="/project">Projets</Link></li>
         <li><Link to="/contact">Contact</Link></li>
