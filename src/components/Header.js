996350fd02ab2a18a858417dd0030f7f01c0bd30
J-D25 <jd.auzanneau@icloud.com>
2022-08-30 13:42:59 +0200

    src/components/Header.js : add burger Menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import './Header.css';
 import {Link, NavLink} from "react-router-dom";
 import Project from "./Project";
@@ -7,12 +8,23 @@ import About from "./About";
 const items = [{name:"Projets", link:"/project", component:{Project}}, {name:"À propos", link:"/about", component:{About}}, {name:"Contact", link:"/contact", component:{Contact}}];
 
 export default function Header(){
+    const [openMenu, setOpenMenu] = useState(false);
+
+    function handleButtonClick(){
+        setOpenMenu(prev => !prev);
+    }
+    function closeMenu (){
+        setOpenMenu(false);
+    }
+    
 return (<>
 <header id="header">
-    <p className="Header-logo"><Link to="/">Jessy-Daniel</Link></p>
-    <nav>
+    <p className="Header-logo"><Link to="/" onClick={() => closeMenu()}>Jessy-Daniel</Link></p>
+    <button className="Header-burger" onClick={handleButtonClick}>Menu</button>
+    <nav className={openMenu ? " openMenu" : ""}>
         <ul>{
-            items.map((item, index) => <li key={index}><NavLink to={item.link}>{item.name}</NavLink></li>)
+            items.map((item, index) => <li key={index}><NavLink to={item.link}
+            onClick={() => closeMenu()}>{item.name}</NavLink></li>)
         }</ul>
     </nav>
 </header>
