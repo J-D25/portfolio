e1a0e22b6740ccdaae59de96ccc4bb0a9e2d0153
J-D25 <jd.auzanneau@icloud.com>
2022-08-30 14:47:23 +0200

    src/components/About.js : add CV link

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,14 @@ import illustration from "../assets/about.svg";
 import MySQL from "../assets/MySQL.svg";
 import VBA from "../assets/VBA.svg";
 import VSCode from "../assets/VSCode.svg";
+import CV from "../data/CV J-D25.pdf"
 
 export default function About() {
   return (<>
     <div className="App-content">
       <h1>À propos</h1>
       <p className="About-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vestibulum bibendum lorem, eu sollicitudin tellus convallis at. Fusce justo massa, aliquet ut maximus a, porttitor vel turpis. Ut faucibus facilisis est, vitae suscipit ante commodo nec. Suspendisse eget ipsum varius, viverra orci sit amet, dapibus leo. Nullam ut lacinia orci, et tincidunt neque.</p>
-      <p><a href="/" className="button">Télécharger mon CV</a></p>
+      <p><a href={CV} className="button">Télécharger mon CV</a></p>
       <div className="About-icons">
         <i className="fa-brands fa-html5"></i>
         <i className="fa-brands fa-css3-alt"></i>
