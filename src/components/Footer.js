e38af7320ee93fab01a718d36afbe66e6f76a331
J-D25 <101711518+J-D25@users.noreply.github.com>
2023-10-11 15:00:43 +0200

    UPD - src/components/Footer.js : Remplacement des valeurs par les variables d'environnement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ return (<>
 <footer id="footer">
     <p className="Footer-copyright">2022, J-D25</p>
     <div className="Footer-socials">
-        <a href="https://github.com/J-D25/" className="Contact-means-socials-link-circle" title="GitHub"><i className="fa-brands fa-github"></i></a>
-        <a href="https://www.linkedin.com/in/jessy-daniel" className="Contact-means-socials-link-circle" title="LinkedIn"><i className="fa-brands fa-linkedin-in"></i></a>
-        <a href="https://www.instagram.com/jd.dev25/" className="Contact-means-socials-link-circle" title="Instagram"><i className="fa-brands fa-instagram"></i></a>
+        {process.env.REACT_APP_SOCIALS_GITHUB && <a href={process.env.REACT_APP_SOCIALS_GITHUB} className="Contact-means-socials-link-circle" title="GitHub"><i className="fa-brands fa-github"></i></a>}
+        {process.env.REACT_APP_SOCIALS_LINKEDIN && <a href={process.env.REACT_APP_SOCIALS_LINKEDIN} className="Contact-means-socials-link-circle" title="LinkedIn"><i className="fa-brands fa-linkedin-in"></i></a>}
+        {process.env.REACT_APP_SOCIALS_INSTAGRAM && <a href={process.env.REACT_APP_SOCIALS_INSTAGRAM} className="Contact-means-socials-link-circle" title="Instagram"><i className="fa-brands fa-instagram"></i></a>}
     </div>
     <ul>{
         items.map((item, index) => <li key={index}><Link to={item.link}>{item.name}</Link></li>)
