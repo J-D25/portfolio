03c1dd883cc820f89f7179175420b2b6d464a088
J-D25 <101711518+J-D25@users.noreply.github.com>
2023-10-20 16:08:32 +0200

    FIX - public/mail.php : Correction des noms de variables

--- public/mail.php
+++ public/mail.php
@@ -8,24 +8,24 @@ $dotenv->safeLoad();
 $receiver_name = $_ENV["REACT_APP_CONTACT_NAME"];
 $receiver_mail = $_ENV["REACT_APP_CONTACT_MAIL"];
 
-    if (isset($_POST['lname']) && isset($_POST['fname']) && isset($_POST['mail']) && isset($_POST['message'])) {
-        if(!empty($_POST['lname']) && !empty($_POST['fname']) && !empty($_POST['mail']) && filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) && !empty($_POST['message'])) {
+    if (isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['mail']) && isset($_POST['message'])) {
+        if(!empty($_POST['lastname']) && !empty($_POST['firstname']) && !empty($_POST['mail']) && filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) && !empty($_POST['message'])) {
             //Variables
-            $firstName = htmlspecialchars($_POST['fname']);
-            $lastName = htmlspecialchars($_POST['lname']);
-            $mailAddress = filter_var($_POST['mail'], FILTER_SANITIZE_EMAIL);
-            $messageContent = htmlspecialchars($_POST['message']);
+            $first_name = htmlspecialchars($_POST['firstname']);
+            $last_name = htmlspecialchars($_POST['lastname']);
+            $mail_address = filter_var($_POST['mail'], FILTER_SANITIZE_EMAIL);
+            $message_content = htmlspecialchars($_POST['message']);
             
             //Contenu mail
-            $sender=$lastName . ' ' . $firstName . ' <' . $mailAddress.'>';
+            $sender=$last_name . ' ' . $first_name . ' <' . $mail_address.'>';
             $header  = 'MIME-Version: 1.0' . "\r\n";
             $header .= 'Content-type: text/html; charset=utf-8' . "\r\n";
             $header .= 'From: '.$sender . "\r\n";
             $object = 'Contact - J-D25';
             $message = '<p>Message envoyé depuis la page Contact du portfolio de J-D25</p>
-            <p><b>Nom et prénom : </b>' . $lastName . ' ' . $firstName . '</p>
-            <p><b>Email : </b>' . $mailAddress . '</p>
-            <p><b>Message : </b>' . $messageContent . '</p>';
+            <p><b>Nom et prénom : </b>' . $last_name . ' ' . $first_name . '</p>
+            <p><b>Email : </b>' . $mail_address . '</p>
+            <p><b>Message : </b>' . $message_content . '</p>';
             $receiver = $receiver_name . ' <' . $receiver_mail . '>';
             $response = mail($receiver, $object, $message, $header);
             if ($response===true){
