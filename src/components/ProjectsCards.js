bbe7a952a2a558f4c91308b1debbf1d2a57ac362
J-D25 <101711518+J-D25@users.noreply.github.com>
2024-06-30 19:13:26 +0200

    FIX - src/components/ProjectsCards.js : Correction title du repo Github

--- src/components/ProjectsCards.js
+++ src/components/ProjectsCards.js
@@ -15,10 +15,10 @@ export default function ProjectsCards(props) {
                     <h2>
                         <a href={link} className="ProjectsCards-content-title" title={title + ", ouvrir dans un nouvel onglet"} target="_blank" rel="noreferrer">{title} <img src={ExternalLink} width="25" height="25" alt="Lien externe" /></a>
                     </h2>
-                    {github !== false ? <a href={github} alt="Repo sur GitHub"><img src={GitHub} width="27" height="25" alt="GitHub" /></a> : null}
+                    {github !== false ? <a href={github} title={"Voir le repo " + title + " sur GitHub"}><img src={GitHub} width="27" height="25" alt="GitHub" /></a> : null}
                 </div>
                 <p>{text}</p>
-                {frontendmentor !== false ? <p>Défi <a href={frontendmentor} alt="Défi Frontend Mentor">Frontend Mentor</a>.</p> : null}
+                {frontendmentor !== false ? <p>Défi <a href={frontendmentor} title="Défi Frontend Mentor">Frontend Mentor</a>.</p> : null}
                 <div className="ProjectsCards-languages">
                     {lang.map((item, index) => <ProjectsLanguages key={index} type="lang" value={item} />)}
                 </div>
