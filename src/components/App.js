3ab85d585f158300123b624a903b331456eb07a2
J-D25 <101711518+J-D25@users.noreply.github.com>
2024-07-01 16:16:25 +0200

    ADD - src/components/App.js : Ajout d'un bouton

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,10 @@ export default function App() {
       <p className="App-hello">Bonjour ! N'hésitez pas à <Link to="/project">découvrir mes projets</Link>, <Link to="/about">en apprendre plus sur moi</Link> ou <Link to="/contact">me contacter</Link>.</p>
       <div className="App-Numbers">
         <Numbers value={dayFromDate("19 May 2000", "years")} title="ans"/>
-        <Numbers value="523" title="commits"/>
+        <Numbers value="1000" title="commits"/>
         <Numbers value={dayFromDate("12 May 2012")} title="jours de code"/>
       </div>
+      <p className="App-hello btn-container"><Link to="/about" className="button">En apprendre plus</Link></p>
     </div>
     </>);
 }
